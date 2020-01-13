5bafe75fd53b2057991427d2bc1a9b88f890a70b
Nicklas Eltvik <nicklas.eltvik@ritc.no>
2020-01-13 09:30:34 +0100

    changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Varden Entrepenør',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'Varden Entreprenør er et entreprenørselskap som ble stiftet i 2008, en seriøs, erfaren og fleksible bedrift',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
